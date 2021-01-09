659dd9d4e489f9f552ed8a10d696ff22b12a3af9
ofkarakus <omerfarukkarakus01@gmail.com>
2021-01-09 16:36:51 +0000

    add create post

--- src/main_app/urls.py
+++ src/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
 urlpatterns = [
     path('', views.display_home_page, name='home'),
+    path('create_post/', views.create_post, name='create_post')
 ]
--- src/main_app/views.py
+++ src/main_app/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.template.defaultfilters import json_script
+from main_app.forms import PostForm
 
 # Create your views here.
 
 
 def display_home_page(request):
     return render(request, 'main_app/home_page.html')
+
+
+def create_post(request):
+    form = PostForm()
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(request.FILES)
+            post = form.save(commit=False)
+            post.author = request.user            
+            post.save()
+            return redirect('main:home')
+    return render(request, 'main_app/create_post.html', context)
